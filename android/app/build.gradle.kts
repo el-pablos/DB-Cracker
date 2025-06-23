9239033f2a778ed9ea05c65ff3b598aed4774b15
el-pablos <yeteprem.end23juni@gmail.com>
2025-06-24 06:26:36 +0700

    fix: remove NDK version requirement to avoid license issues

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.db_cracker"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    // ndkVersion = "27.0.12077973" // Commented out to avoid license issues
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
